48d021dd655e4f0a0f6adde480444368a66353c4
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2023-09-05 09:08:36 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardGame.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
