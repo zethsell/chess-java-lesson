42c673179084769ef81aa7e106964200b7ca987a
Marcio T Rodrigues <marcio.rodrigues@boxti.com.br>
2023-09-05 09:49:08 -0300

    chess layer and printing board

--- src/boardGame/Board.java
+++ src/boardGame/Board.java
@@ -8,7 +8,7 @@ public class Board {
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
-        pieces = new Piece[rows][columns]
+        pieces = new Piece[rows][columns];
     }
 
     public int getRows() {
@@ -25,5 +25,13 @@ public class Board {
 
     public void setColumns(int columns) {
         this.columns = columns;
+    }
+
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
+    }
+
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
